1fab6f5efd46ee0e009f5b47d7216addb008ae85
aquiltanveeer <akhil.tanveer@gmail.com>
2025-04-09 03:15:11 +0530

    Add files via upload

---
+++ Aquil/CustomOrderProcessing/Controller/Adminhtml/Orders/CustomOrders.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Aquil\CustomOrderProcessing\Controller\Adminhtml\Orders;
+
+class CustomOrders extends \Magento\Backend\App\Action
+{
+	protected $resultPageFactory = false;
+
+	public function __construct(
+		\Magento\Backend\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $resultPageFactory
+	)
+	{
+		parent::__construct($context);
+		$this->resultPageFactory = $resultPageFactory;
+	}
+
+	public function execute()
+	{
+		$resultPage = $this->resultPageFactory->create();
+		$resultPage->getConfig()->getTitle()->prepend((__('Custom Orders Grid')));
+
+		return $resultPage;
+	}
+
+
+}
